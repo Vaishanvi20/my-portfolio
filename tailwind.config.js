73e488b01f87314f3c69e9b312c89a35d994f0a5
Vaishnavi Katkar <115980995+Vaishanvi20@users.noreply.github.com>
2024-11-24 16:16:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Adjust the path if your files are located elsewhere
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
